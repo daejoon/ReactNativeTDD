8f0203b518f924ed4db6a0ed23befa9bc20f0bd9
daejoon <kkode1911@gmail.com>
2019-06-18 09:37:17 +0900

    App.js 테스트 케이스 작성

--- test/App.spec.js
+++ test/App.spec.js
@@ -8,20 +8,18 @@ import {shallow} from 'enzyme';
 
 import App from '../src/App';
 
-describe('Jest', () => {
-    it('is it working ?', () => {
-        const a = 1;
+describe('App', () => {
+    const wrapper = shallow(<App></App>);
 
-        expect(a + 1).toBe(2);
+    it('is Text visible ?', () => {
+        expect(wrapper.find('Text').contains('ToDo TDD')).toBe(true);
     });
-});
 
+    it('is AddToDo visible ?', () => {
+        expect(wrapper.find('AddToDo')).toHaveLength(1);
+    });
 
-describe('Enzyme', () => {
-    it('is it working ?', () => {
-        const text = 'some text';
-        const wrapper = shallow(<Text>{text}</Text>);
-
-        expect(wrapper.props().children).toBe(text);
+    it('is ToDoList visible ?', () => {
+        expect(wrapper.find('ToDoList')).toHaveLength(1);
     });
 });
